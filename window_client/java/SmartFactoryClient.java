a5d6d306ae25afc55a90b5e74f96ec1ac26bd60a
inhawinter <inhawinter@gmail.com>
2023-07-20 00:28:41 +0900

    java web client v0.4

--- window_client/java/SmartFactoryClient.java
+++ window_client/java/SmartFactoryClient.java
@@ -1,38 +1,83 @@
+// window monitoring app v0.1
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;  // Java 11
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SmartFactoryClient {
+    private static JLabel ipLabel;
+    private static JLabel temperatureLabel;
+
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Inha Smart Factory!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 150);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 1));  // row, column
+
+        ipLabel = new JLabel("IP Address: ");
+        temperatureLabel = new JLabel("Temperature: ");
+
+        JButton refreshButton = new JButton("Check Temperature!");
+        refreshButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateLabels();
+            }
+        });
+
+        panel.add(ipLabel);
+        panel.add(temperatureLabel);
+        panel.add(refreshButton);
+
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+    }
+
+    private static void updateLabels() {
         String urlStr = "http://192.168.45.56:80/";
-
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create(urlStr))
-                .build();
-
         try {
-            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            URL url = new URL(urlStr);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
 
-            if (response.statusCode() == 200) {
-                String[] lines = response.body().split("\n");
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                    response.append("\n");
+                }
+                reader.close();
+
+                String[] lines = response.toString().split("\n");
                 if (lines.length >= 4) {
-                    String ipLine = lines[2];
-                    //                    String temperatureLine = lines[3];
-                    String ip_address = ipLine.split(": ")[1];
-                    //                    String temperature = temperatureLine.split(": ")[1];
+                    String ip_address = getValue(lines[2]);
 
-                    System.out.println("IP address : " + ip_address);
-                    System.out.println(lines[3]);
+                    ipLabel.setText("IP address : " + ip_address);
+                    temperatureLabel.setText(lines[3]);
                 } else {
-                    System.out.println("Response format error!");
+                    JOptionPane.showMessageDialog(null, "Response format error!");
                 }
             } else {
-                System.out.println("Http connection failed : " + response.statusCode());
+                JOptionPane.showMessageDialog(null, "Http connection failed : " + responseCode);
             }
-        } catch (IOException | InterruptedException e) {
+            connection.disconnect();
+        } catch (IOException e) {
             e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Exception occured." + e.getMessage());
         }
+    }
+
+    private static String getValue(String line) {
+        return line.split(": ")[1];
     }
 }
