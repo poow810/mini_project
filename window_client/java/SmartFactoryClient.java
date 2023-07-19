8dd50ada687b1abfaa674cc41378cc13459c75ac
inhawinter <inhawinter@gmail.com>
2023-07-20 00:05:07 +0900

    java web client v0.3

--- window_client/java/SmartFactoryClient.java
+++ window_client/java/SmartFactoryClient.java
@@ -1,48 +1,37 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
+import java.net.URI;
+import java.net.http.HttpClient;  // Java 11
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 public class SmartFactoryClient {
     public static void main(String[] args) {
-        String urlStr = "http://192.168.15.245";
+        String urlStr = "http://192.168.45.56:80/";
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(urlStr))
+                .build();
 
         try {
-            URL url = new URL(urlStr);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
-            int responseCode = connection.getResponseCode(); // status code
-            if(responseCode == HttpURLConnection.HTTP_OK){  //if(responseCode == 200){
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
-                        response.append("\n");
-                    }
+            if (response.statusCode() == 200) {
+                String[] lines = response.body().split("\n");
+                if (lines.length >= 4) {
+                    String ipLine = lines[2];
+                    //                    String temperatureLine = lines[3];
+                    String ip_address = ipLine.split(": ")[1];
+                    //                    String temperature = temperatureLine.split(": ")[1];
 
-                    String[] lines = response.toString().split("\n");
-                    if (lines.length >= 4) {
-                        String ipLine = lines[2];
-//                    String temperatureLine = lines[3];
-                        String ip_address = ipLine.split(": ")[1];
-//                    String temperature = temperatureLine.split(": ")[1];
-
-                        System.out.println("IP address : " + ip_address);
-                        //System.out.println("Temperature : " + temperature);
-                        System.out.println(lines[3]);
-                    } else {
-                        System.out.println("Response format error!");
-                    }
+                    System.out.println("IP address : " + ip_address);
+                    System.out.println(lines[3]);
+                } else {
+                    System.out.println("Response format error!");
                 }
-            }else{
-                System.out.println("Http connection failed : " + responseCode);
+            } else {
+                System.out.println("Http connection failed : " + response.statusCode());
             }
-            connection.disconnect();
-        }
-        catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
