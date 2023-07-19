fad992a51ffc4774c5d96d2c93182f5cfef8bc84
inhawinter <inhawinter@gmail.com>
2023-07-19 13:54:12 +0900

    webserver v0.3

--- web_server/webserver.ino
+++ web_server/webserver.ino
@@ -9,6 +9,14 @@ const char* password = "inha4885";
 OLED_U8G2 oled; // create oled object
 WebServer server(80);  // create WebServer object, port
 
+int tempSensor = A2; // temperature sensor
+// formula
+int Vo;
+double R1 = 10000;
+double logR2, R2, T, Tc;
+double c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+double Tf = 0;
+
 void setup(void) {
   Serial.begin(115200);  // ESP32 baud rate
   oled.setup();
@@ -43,5 +51,16 @@ void loop(void) {
 
 void handleRootEvent() {
   Serial.println("main page");
-  server.send(200, "text/plain", "Welcome Inha SmartFactory WebServer!");  // status code 200(OK), format, message
+
+  Vo = analogRead(tempSensor); // read from temperature sensing value
+  R2 = R1 * (4095.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  Tc = T - 273.15;  // celsius
+  Tf = (Tc * 9.0/5.0) + 32.0;  // fahrenheit
+
+  String message = "Welcome Inha SmartFactory WebServer!\n\n";
+  message = message + "Temperature: " + String(Tc) + "C " + "(" + String(Tf) + "F)";
+  server.send(200, "text/plain", message);  // status code 200(OK), format, message
+  Serial.println(message); // monitoring
 }
