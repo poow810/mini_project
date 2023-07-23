c6eac1a8a60d52c0bc1750c518eeda44b4407eab
poow810 <poow810@naver.com>
2023-07-23 14:29:34 +0900

    온도 측정 버튼 기능 추가

--- temperatureSensor/src/main/java/com/hawoon/temperatureSensor/controller/TemperatureController.java
+++ temperatureSensor/src/main/java/com/hawoon/temperatureSensor/controller/TemperatureController.java
@@ -2,15 +2,17 @@ package com.hawoon.temperatureSensor.controller;
 
 import com.hawoon.temperatureSensor.TemperatureDto;
 import com.hawoon.temperatureSensor.TemperatureService;
+import lombok.experimental.PackagePrivate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@RestController
+@Controller
 public class TemperatureController {
 
     private final TemperatureService temperatureService;
@@ -27,7 +29,20 @@ public class TemperatureController {
     }
 
     //
+//    @GetMapping("/measure")
+//    public List<TemperatureDto> getMeasuredTemperature() {
+//        return temperatureService.getTemperature();
+//    }
+
     @GetMapping("/measure")
+    public String showTemperatureData(Model model) {
+        List<TemperatureDto> data = temperatureService.getTemperature();
+        model.addAttribute("data", data);
+        return "test/temperature";
+    }
+
+    @GetMapping("/measure/data")
+    @ResponseBody
     public List<TemperatureDto> getMeasuredTemperature() {
         return temperatureService.getTemperature();
     }
